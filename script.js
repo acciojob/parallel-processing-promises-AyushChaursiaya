4ed4709b7fae173ce928bfae4d4ebf8545f079aa
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-14 23:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,30 +7,38 @@ const images = [
   { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-function downloadImages() {
-    const promises = images.map(imageObj => {
-        return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.onload = () => resolve(img);
-            img.onerror = () => {
-                console.error(`Failed to load image's URL: ${imageObj.url}`);
-                resolve(null); // Resolve with null if image loading fails
-            };
-            img.src = imageObj.url;
-        });
-    });
 
-    Promise.all(promises)
-    .then(images => {
-        images.forEach(img => {
-            if (img) {
-                output.appendChild(img);
-            }
-        });
-    })
-    .catch(error => {
-        console.error(error);
+btn.addEventListener("click", () => {
+    images.forEach(imageObj => {
+        const img = new Image();
+        img.onload = () => output.appendChild(img);
+        img.src = imageObj.url;
     });
-}
+});
+// function downloadImages() {
+//     const promises = images.map(imageObj => {
+//         return new Promise((resolve, reject) => {
+//             const img = new Image();
+//             img.onload = () => resolve(img);
+//             img.onerror = () => {
+//                 console.error(`Failed to load image's URL: ${imageObj.url}`);
+//                 resolve(null); // Resolve with null if image loading fails
+//             };
+//             img.src = imageObj.url;
+//         });
+//     });
 
-btn.addEventListener("click", downloadImages);
+//     Promise.all(promises)
+//     .then(images => {
+//         images.forEach(img => {
+//             if (img) {
+//                 output.appendChild(img);
+//             }
+//         });
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// }
+
+// btn.addEventListener("click", downloadImages);
