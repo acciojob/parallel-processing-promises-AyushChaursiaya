2e79a25fcf9b1f55e673af5784da1ab5dfeea9c9
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-14 23:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImages() {
+    Promise.all(images.map(imageObj => {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => {
+                output.appendChild(img);
+                resolve(img);
+            };
+            img.onerror = () => reject(new Error(`Failed to load image's URL: ${imageObj.url}`));
+            img.src = imageObj.url;
+        });
+    }))
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+btn.addEventListener("click", downloadImages);
